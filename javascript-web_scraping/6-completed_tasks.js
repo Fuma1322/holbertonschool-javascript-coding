81a7ebd0aa909df4822902b8775ccc12c8c66301
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-05-14 13:31:20 +0200

    Update 6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
+#!/usr/bin/node
 
+const request = require('request');
+const url = process.argv[2];
+
+request(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const completed = {};
+
+  body.forEach(task => {
+    if (task.completed) {
+      completed[task.userId] = (completed[task.userId] || 0) + 1;
+    }
+  });
+  console.log(completed);
+});
