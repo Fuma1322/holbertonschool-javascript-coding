428d5498ee764bb79c53eef847c23783ddd015e2
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-05-14 13:09:55 +0200

    Update 1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
+#!/usr/bin/node
 
+const fs = require('fs');
+const fileName = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(fileName, data, 'utf-8', (error) => {
+  if (error) {
+    console.error(error);
+  }
+});
