b92d5d5231a656e6cab01b490fcee77d1fcdb210
Fuma1322 <leonardfuma26@gmail.com>
2024-05-14 12:44:09 +0200

    js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,29 +1,11 @@
-#!/usr/bin/env node
-
-// Import the fs module to interact with the file system
+#!/usr/bin/node
+// a script that writes a string to a file.
 const fs = require('fs');
-const path = require('path');
-
-// Get the file path from the command line arguments
-const filePath = process.argv[2];
-
-// Function to read and print the file content
-function readFile(filePath) {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            // If an error occurs, print the error object
-            console.error(err);
-        } else {
-            // If no error, print the file content
-            console.log(data);
-        }
-    });
-}
-
-// Check if the file path is provided
-if (!filePath) {
-    console.log("Please provide a file path as an argument.");
-} else {
-    // Call the function with the provided file path
-    readFile(filePath);
-}
+const fileName = process.argv[2];
+fs.readFile(fileName, 'utf8', (error, data) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
+});
