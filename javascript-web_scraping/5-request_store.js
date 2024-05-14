2eb28d924501bcae89a4ad1097c7bcbf4972373c
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-05-14 13:30:45 +0200

    Update 5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,18 @@
+#!/usr/bin/node
 
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const fileName = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(fileName, body, 'utf-8', function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
