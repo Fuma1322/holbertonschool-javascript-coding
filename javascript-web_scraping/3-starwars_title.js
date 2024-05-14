fd840a278a57f2c1b2327d351a6a876dc4ebe46f
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-05-14 13:20:53 +0200

    Update 3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,15 @@
+#!/usr/bin/node
 
+const request = require('request');
+const movieid = process.argv[2];
+let url = 'https://swapi-api.hbtn.io/api/films/';
+
+url = url.concat(movieid);
+request(url, function (err, reposne, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
+});
