ac235477756671cbc04354b7a14a21a53c7d5ea5
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-05-14 13:25:41 +0200

    Update 4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,23 @@
+#!/usr/bin/node
 
+const request = require('request');
+const url = process.argv[2];
+
+let count = 0;
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const results = JSON.parse(body).results;
+    for (const result of results) {
+      const characters = result.characters;
+      for (const character of characters) {
+        if (character.includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
