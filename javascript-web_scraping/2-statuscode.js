08dfa1784784df979395e97ac594eabd86a20c68
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-05-14 13:10:35 +0200

    Update 2-statuscode.js

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
+const url = process.argv[2];
+const request = require('request');
 
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
