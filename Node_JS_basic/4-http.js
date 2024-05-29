56da80744a767fb26225b1e4c77e61efc89d4ad6
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-05-29 12:54:25 +0200

    Update 4-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,1 +1,19 @@
+const http = require('http');
 
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (_, response) => {
+  const responseText = 'Hello Holberton School!';
+  response.setHeader('Content-Type', 'text/plain');
+  response.setHeader('Content-Length', responseText.length);
+  response.statusCode = 200;
+  response.write(Buffer.from(responseText));
+});
+
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
+
+module.exports = app;
