1799fd08c59556f103e70cd4f834dde63c5e4e29
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-05-29 13:05:37 +0200

    Update 7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,1 +1,56 @@
+/* eslint-disable */
+const express = require('express');
+const fs = require('fs').promises;
 
+function countStudents(path) {
+  return fs
+    .readFile(path, 'utf8')
+    .then((data) => {
+      const rows = data.split('\n').filter((row) => row);
+      const headers = rows.shift().split(',');
+      const fieldIndex = headers.indexOf('field');
+      const firstNameIndex = headers.indexOf('firstname');
+      const fields = [
+        ...new Set(rows.map((row) => row.split(',')[fieldIndex])),
+      ];
+
+      let result = `Number of students: ${rows.length}\n`;
+
+      fields.forEach((field) => {
+        const students = rows.filter(
+          (row) => row.split(',')[fieldIndex] === field
+        );
+        result += `Number of students in ${field}: ${
+          students.length
+        }. List: ${students
+          .map((student) => student.split(',')[firstNameIndex])
+          .join(', ')}\n`;
+      });
+      return result;
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
+}
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students\n${err.message}`);
+    });
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+
+module.exports = app;
