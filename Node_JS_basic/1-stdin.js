0695460605d3aeb2217fd4dc6bc66f0ba8cd17b1
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-05-29 12:34:25 +0200

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,1 +1,9 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name.toString()}`);
+});
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
+});
